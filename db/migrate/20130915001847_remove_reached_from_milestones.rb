48fbaca9e744f0e4a8ecad4ab5c1ceb810584e89
Kelvin Hung <kelvin_hung@intuit.com>
2013-12-25 23:59:27 -0800

    Migration fix

--- db/migrate/20130915001847_remove_reached_from_milestones.rb
+++ db/migrate/20130915001847_remove_reached_from_milestones.rb
@@ -1,9 +1,9 @@
 class RemoveReachedFromMilestones < ActiveRecord::Migration
   def up
-    remove_column :milestones, :Reached
+    remove_column :milestones, :reached
   end
 
   def down
-    add_column :milestones, :Reached, :boolean
+    add_column :milestones, :reached, :boolean
   end
 end
