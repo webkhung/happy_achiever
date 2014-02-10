eb4954054d4d510f15d99561f6df7d7129c123f1
Kelvin Hung <kelvin_hung@intuit.com>
2014-02-09 23:14:54 -0800

    Changed mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Secret2::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'happyachiever.herokuapp.com' } #todo Need to change on production
 
   # Enable threaded mode
   # config.threadsafe!
