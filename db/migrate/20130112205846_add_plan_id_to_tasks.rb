bceeffceb969b8d8da268330e0c8bcddd3f5ddc1
Kelvin Hung <kelvin_hung@intuit.com>
2014-02-09 20:02:46 -0800

    .

--- db/migrate/20130112205846_add_plan_id_to_tasks.rb
+++ db/migrate/20130112205846_add_plan_id_to_tasks.rb
@@ -2,10 +2,10 @@ class AddPlanIdToTasks < ActiveRecord::Migration
   def change
     add_column :tasks, :plan_id, :integer
 
-    FocusArea.all.each do |fa|
-      fa.tasks.each do |t|
-        t.update_column(:plan_id, fa.plan_id)
-      end
-    end
+    #FocusArea.all.each do |fa|
+    #  fa.tasks.each do |t|
+    #    t.update_column(:plan_id, fa.plan_id)
+    #  end
+    #end
   end
 end
