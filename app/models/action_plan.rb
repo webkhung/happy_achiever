647cea8f2902be7de0476587feee3f3441c548a9
Kelvin Hung <kelvin_hung@intuit.com>
2014-02-11 00:51:35 -0800

    typo

--- app/models/action_plan.rb
+++
@@ -1,6 +0,0 @@
-class ActionPlan < ActiveRecord::Base
-  attr_accessible :task_id, :occurrence, :target_count, :start_date
-
-  belongs_to :task
-  has_many :schedules
-end
--- app/models/goal.rb
+++
@@ -1,4 +0,0 @@
-class Goal < ActiveRecord::Base
-  attr_accessible :content, :plan_id
-  belongs_to :plan
-end
