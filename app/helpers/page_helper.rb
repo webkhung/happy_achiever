35109ee76354fb84fb386c5fd8c7054ee3e57a63
Kelvin Hung <kelvin_hung@intuit.com>
2014-02-27 22:57:38 -0800

    Added quote and trying to fix Postgre

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,2 +1,28 @@
 module PageHelper
+  def quote()
+    [
+      "&#8220;I have not failed. I've just found 10,000 ways that won't work.&#8221; ― Thomas Edison",
+      "&#8220;Success is not final, failure is not fatal: it is the courage to continue that counts.&#8221; ― Winston Churchill",
+      "&#8220;Success is most often achieved by those who don\'t know that failure is inevitable.&#8221; ― Coco Chanel",
+      "&#8220;Only those who dare to fail greatly can ever achieve greatly.&#8221; ― Robert F. Kennedy",
+      "&#8220;Failure is the condiment that gives success its flavor.&#8221; ― Truman Capote",
+      "&#8220;The way to get started is to quit talking and begin doing.&#8221; ― Walt Disney Company",
+      "&#8220;Do you want to know who you are? Don\'t ask. Act! Action will delineate and define you.&#8221; - Thomas Jefferson",
+      "&#8220;When someone tells me \"no,\" it doesn\'t mean I can't do it, it simply means I can't do it with them.&#8221; -  Karen E. Quinones Miller",
+      "&#8220;The only thing standing between you and your goal is the bullshit story you keep telling yourself as to why you can\'t achieve it.&#8221; - Jordan Belfort",
+      "&#8220;All great achievements require time.&#8221; - Maya Angelou",
+      "&#8220;I\'ve come to believe that all my past failure and frustration were actually laying the foundation for the understandings that have created the new level of living I now enjoy.&#8221; - Tony Robbins",
+      "&#8220;Success comes from taking the initiative and following up... persisting... eloquently expressing the depth of your love. What simple action could you take today to produce a new momentum toward success in your life?&#8221; - Tony Robbins",
+      "&#8220;If we want to direct our lives, we must take control of our consistent actions. It\'s not what we do once in a while that shapes our lives, but what we do consistently.&#8221; - Tony Robbins",
+      "&#8220;Once you have mastered time, you will understand how true it is that most people overestimate what they can accomplish in a year - and underestimate what they can achieve in a decade!&#8221; - Tony Robbins",
+      "&#8220;Change is never a matter of ability. Change is always a matter of motivation.&#8221; - Tony Robbins",
+      "&#8220;A real decision is measured by the fact that you\'ve taken a new action. If there\'s no action, you haven\'t truly decided.&#8221; - Tony Robbins",
+      "&#8220;It is in your moments of decision that your destiny is shaped.&#8221; - Tony Robbins",
+      "&#8220;If you do what you\'ve always done, you\'ll get what you\'ve always gotten.&#8221; - Tony Robbins",
+      "&#8220;For changes to be of any true value, they\'ve got to be lasting and consistent.&#8221; - Tony Robbins",
+      "&#8220;Stay committed to your decisions, but stay flexible in your approach.&#8221; - Tony Robbins",
+      "&#8220;We don't need to wait to be happy after we achieved our goals. We can feel happy while we are working on the goal as long as we are growing, learning and enjoy the process.&#8221; - Tony Robbins"
+    ].sample.html_safe
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,13 +31,10 @@ class User < ActiveRecord::Base
   end
 
   def votable_received
-    plans = Plan.find_by_sql("SELECT DISTINCT `plans`.* FROM `plans` INNER JOIN `votes` ON `votes`.`votable_id` = `plans`.`id` AND `votes`.`votable_type` = 'Plan' WHERE `plans`.`state` = 'live' AND `plans`.`user_id` = #{self.id}").each {|p| puts p.upvotes.size }
-    achievements = Achievement.find_by_sql("SELECT DISTINCT `achievements`.* FROM `achievements` INNER JOIN `votes` ON `votes`.`votable_id` = `achievements`.`id` AND `votes`.`votable_type` = 'Achievement' WHERE `achievements`.`user_id` = #{self.id}").each {|p| puts p.upvotes.size }
+    plans = Plan.find_by_sql("SELECT DISTINCT plans.* FROM plans INNER JOIN votes ON votes.votable_id = plans.id AND votes.votable_type = 'Plan' WHERE plans.state = 'live' AND plans.user_id = #{self.id}").each {|p| puts p.upvotes.size }
+    achievements = Achievement.find_by_sql("SELECT DISTINCT achievements.* FROM achievements INNER JOIN votes ON votes.votable_id = achievements.id AND votes.votable_type = 'Achievement' WHERE achievements.user_id = #{self.id}").each {|p| puts p.upvotes.size }
 
     plans.concat achievements
   end
 
-  #def
-  #  Achievement.find_by_sql("SELECT `achievements`.* FROM `achievements` INNER JOIN `votes` ON `votes`.`votable_id` = `achievements`.`id` AND `votes`.`votable_type` = 'Achievement' WHERE `achievements`.`user_id` = 7").each {|p| puts p.upvotes.size }
-  #end
 end
