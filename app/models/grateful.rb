8798567c363eec6ca2707d1ccbedee4959975fbb
Kelvin Hung <kelvin_hung@intuit.com>
2014-04-12 17:18:20 -0700

    Fixed grateful

--- app/models/grateful.rb
+++ app/models/grateful.rb
@@ -7,9 +7,9 @@ class Grateful < ActiveRecord::Base
   belongs_to :achievement
   belongs_to :user
 
-  def self.all_gratefuls
-    arr = []
-    Grateful.all.each {|g| arr << g.grateful_1 << g.grateful_2 << g.grateful_3 << g.grateful_4 << g.grateful_5 }
-    arr.reject!{ |r|r.blank? }.uniq
-  end
+  #def self.all_gratefuls
+  #  arr = []
+  #  Grateful.all.each {|g| arr << g.grateful_1 << g.grateful_2 << g.grateful_3 << g.grateful_4 << g.grateful_5 }
+  #  arr.reject!{ |r|r.blank? }.uniq
+  #end
 end
